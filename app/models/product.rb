9e02af81a96f19f3b813e4734cdb13fc7bdbc19b
Andre Oliveira <desmonplays@gmail.com>
2019-02-14 19:46:28 -0400

    creating the joins table between products and categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :category
 
+  validates :name, presence: true
+  validates :price, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_233400) do
+ActiveRecord::Schema.define(version: 2019_02_14_234513) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,14 +18,18 @@ ActiveRecord::Schema.define(version: 2019_02_14_233400) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories_products", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
+    t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id"
+  end
+
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  add_foreign_key "products", "categories"
 end
