cde06fcdfa4ab8ebe7d7b73879bc7b476fa4e215
Andre Oliveira <desmonplays@gmail.com>
2019-02-14 22:48:34 -0400

    (fix_bug) translating the error messages

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module BackendTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.available_locales = [:en, :"pt-BR"]
+    config.i18n.default_locale = :"pt-BR"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
