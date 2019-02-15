0aae098cadfbfff9c3755f59b337e88e6bcc9b6f
Andre Oliveira <desmonplays@gmail.com>
2019-02-15 15:51:25 -0400

    (fix_bug) fixing migrations bugs with id is bigint

--- db/schema.rb
+++ db/schema.rb
@@ -20,16 +20,18 @@ ActiveRecord::Schema.define(version: 2019_02_14_234513) do
 
   create_table "categories_products", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "product_id", null: false
-    t.bigint "category_id", null: false
+    t.integer "category_id", null: false
     t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
     t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id"
   end
 
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "products", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "categories_products", "categories", column: "product_id"
+  add_foreign_key "categories_products", "products", column: "category_id"
 end
