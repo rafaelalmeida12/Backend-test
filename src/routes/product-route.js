bc17277223b924311bfb2d6889b488ad7575a2e1
Thiago <itsadeadh2@gmail.com>
2019-05-01 17:27:22 -0400

    feat: implementa rotas para os metodos do controller

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -3,3 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
+
+router.get('/', controller.get);
+router.get('/id/:id', controller.getById);
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
