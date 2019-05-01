ba6701e93735bcf55f85eb40fb70b8d643c36583
Thiago <itsadeadh2@gmail.com>
2019-05-01 17:54:39 -0400

    feat: implementa o metodo get all no controller

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const repository = require('../repository/category-repository');
+
+exports.get = (req, res, next) => {
+    repository
+    .get()
+    .then(data => {
+        res.status(200).send(data);
+    })
+    .catch(e => {
+        res.status(400).send(e);
+    });
+};
