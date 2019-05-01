e8c2eb30d326c2b58ea1b345114c301f74d47a0e
Thiago <itsadeadh2@gmail.com>
2019-05-01 17:03:33 -0400

    feat: configura servidor para escutar na porta determinada

--- bin/server.js
+++ bin/server.js
@@ -6,6 +6,12 @@ const http = require('http');
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log("API rodando na porta " + port);
+
 function normalizePort(val) {
     const port = parseInt(val, 10);
 
