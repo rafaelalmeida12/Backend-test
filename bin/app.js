de9e0849ced7b5663b227ff9741c2136c7cfaa4a
Thiago <itsadeadh2@gmail.com>
2019-05-01 16:43:05 -0400

    feat: adiciona dependencias ao app.js e o exporta

---
+++ bin/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+module.exports = app;
