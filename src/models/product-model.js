1280c5caaef6d7d02d6f8eb40c5bab35ebdd65c0
Thiago <itsadeadh2@gmail.com>
2019-05-01 17:10:19 -0400

    feat: implementa o schema de produto

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -2,3 +2,14 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    preco: {
+        type: Number,
+        required: true
+    }
+});
