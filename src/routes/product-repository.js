251034da25fd7591f184d801a590948306dbba4d
Thiago <itsadeadh2@gmail.com>
2019-05-01 17:14:32 -0400

    feat: implementa o metodo get all

--- src/routes/product-repository.js
+++ src/routes/product-repository.js
@@ -2,3 +2,8 @@
 const mongoose = require('mongoose');
 const _ = require('lodash');
 const Product = mongoose.model('Product');
+
+exports.get = (req, res, next) => {
+    return Product
+        .find({}, 'nome preco')
+}
