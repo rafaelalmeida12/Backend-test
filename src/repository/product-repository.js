35d7413fd18aa17c58b99ff6547714a4fc43f8db
Thiago <itsadeadh2@gmail.com>
2019-05-01 18:24:58 -0400

    feat: implementa a populacao das categorias nos produtos

--- src/repository/product-repository.js
+++ src/repository/product-repository.js
@@ -5,15 +5,23 @@ const Product = mongoose.model('Product');
 
 exports.get = (req, res, next) => {
     return Product
-        .find({}, 'nome preco')
+        .find({}, 'nome preco categorias')
+        .populate('categorias');
 }
 
 exports.getById = (id) => {
     return Product
         .findById(id)
+        .populate('categorias');
 }
 
-exports.post = (objProduct) => {
+exports.getByCategory = (id) => {
+    return Product
+        .find({categorias: id})
+        .populate('categorias');
+}
+
+exports.post = (objProduct) => {    
     var product = new Product(objProduct);
     return product
         .save();
