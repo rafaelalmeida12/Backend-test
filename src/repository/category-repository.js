2736cf2d6b1ca4170f72dff5c6a43143892ed104
Thiago <itsadeadh2@gmail.com>
2019-05-01 17:45:07 -0400

    feat: implementa o metodo get all

--- src/repository/category-repository.js
+++ src/repository/category-repository.js
@@ -1,3 +1,8 @@
 'use strict';
 const mongoose = require('mongoose');
 const Category = mongoose.model('Category');
+
+exports.get = (req, res, next) => {
+    return Category
+        .find({}, 'nome');
+}
