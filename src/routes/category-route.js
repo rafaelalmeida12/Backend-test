c33998405351d16435450b30e6cc1ffe8e79d516
Thiago <itsadeadh2@gmail.com>
2019-05-01 17:58:22 -0400

    feat: exporta as rotas

--- src/routes/category-route.js
+++ src/routes/category-route.js
@@ -9,3 +9,5 @@ router.get('/id/:id', controller.getById);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
+
+module.exports = router;
